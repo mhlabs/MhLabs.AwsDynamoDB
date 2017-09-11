7c5ca3a5af9ad37804b712da08e40c20e7ad39b8
satkumind <satyendra.kumar@indpro.se>
2017-09-11 20:40:29 +0530

    adding DynamoDBOperationConfig for table name

--- MhLabs.AwsDynamoDB/DynamoDBRepository.cs
+++ MhLabs.AwsDynamoDB/DynamoDBRepository.cs
@@ -15,43 +15,58 @@ namespace MhLabs.AwsDynamoDB
             this.client = client ?? new AmazonDynamoDBClient();
         }
 
-        public async Task DeleteAsync(T obj)
+        public async Task DeleteAsync(T obj, string overrideTableName = null)
         {
             using (var dbContext = new DynamoDBContext(client))
             {
-                await dbContext.DeleteAsync(obj);
+                if (!string.IsNullOrEmpty(overrideTableName))
+                    await dbContext.DeleteAsync(obj, new DynamoDBOperationConfig { OverrideTableName = overrideTableName });
+                else
+                    await dbContext.DeleteAsync(obj);
             }
         }
 
-        public async Task<List<T>> FromQueryAsync(QueryOperationConfig queryOperationConfig, DynamoDBOperationConfig operationConfig = null)
+        public async Task<List<T>> FromQueryAsync(QueryOperationConfig queryOperationConfig, string overrideTableName = null)
         {
             using (var dbcontext = new DynamoDBContext(client))
             {
-                return await dbcontext.FromQueryAsync<T>(queryOperationConfig, operationConfig: operationConfig).GetNextSetAsync();
+                if (!string.IsNullOrEmpty(overrideTableName))
+                    return await dbcontext.FromQueryAsync<T>(queryOperationConfig, new DynamoDBOperationConfig { OverrideTableName = overrideTableName }).GetNextSetAsync();
+                else
+                    return await dbcontext.FromQueryAsync<T>(queryOperationConfig).GetNextSetAsync();
             }
         }
 
-        public async Task<List<T>> QueryAsync(object hashKeyValue)
+        public async Task<List<T>> QueryAsync(object hashKeyValue, string overrideTableName = null)
         {
             using (var dbcontext = new DynamoDBContext(client))
             {
-                return await dbcontext.QueryAsync<T>(hashKeyValue).GetNextSetAsync();
+                if (!string.IsNullOrEmpty(overrideTableName))
+                    return await dbcontext.QueryAsync<T>(hashKeyValue, new DynamoDBOperationConfig { OverrideTableName = overrideTableName }).GetNextSetAsync();
+                else
+                    return await dbcontext.QueryAsync<T>(hashKeyValue).GetNextSetAsync();
             }
         }
 
-        public async Task SaveAsync(T obj)
+        public async Task SaveAsync(T obj, string overrideTableName = null)
         {
             using (var dbContext = new DynamoDBContext(client))
             {
-                await dbContext.SaveAsync(obj);
+                if (!string.IsNullOrEmpty(overrideTableName))
+                    await dbContext.SaveAsync(obj, new DynamoDBOperationConfig { OverrideTableName = overrideTableName });
+                else
+                    await dbContext.SaveAsync(obj);
             }
         }
 
-        public async Task<List<T>> ScanAsync(IEnumerable<ScanCondition> conditions)
+        public async Task<List<T>> ScanAsync(IEnumerable<ScanCondition> conditions, string overrideTableName = null)
         {
             using (var dbcontext = new DynamoDBContext(client))
             {
-                return await dbcontext.ScanAsync<T>(conditions).GetNextSetAsync();
+                if (!string.IsNullOrEmpty(overrideTableName))
+                    return await dbcontext.ScanAsync<T>(conditions, new DynamoDBOperationConfig { OverrideTableName = overrideTableName }).GetNextSetAsync();
+                else
+                    return await dbcontext.ScanAsync<T>(conditions).GetNextSetAsync();
             }
         }
     }
