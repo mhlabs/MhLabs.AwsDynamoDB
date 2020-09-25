431cabf3899b57982a9b5631e5258bed52820117
Marcus Casserfelt <marcus.casserfelt@gmail.com>
2020-09-25 08:48:01 +0200

    fix: Added converter for enums

---
+++ MhLabs.AwsDynamoDB.Tests/DynamoEnumConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.DynamoDBv2.DocumentModel;
+using MhLabs.AwsDynamoDB.Converters;
+using System;
+using Xunit;
+
+namespace MhLabs.AwsDynamoDB.Tests
+{
+    public class DynamoEnumConverterTests
+    {
+        [Fact]
+        public void Should_Create_Entry_From_Enum()
+        {
+            var dayOfWeek = DayOfWeek.Friday;
+            var converter = new DynamoEnumConverter<DayOfWeek>();
+            var entry = converter.ToEntry(dayOfWeek);
+            Assert.Equal("Friday", (string)((Primitive)entry).Value);
+        }
+
+        [Fact]
+        public void Should_Create_Enum_From_Entry()
+        {
+            DynamoDBEntry entry = new Primitive { Value = "Friday" };
+            var converter = new DynamoEnumConverter<DayOfWeek>();
+            var dayOfWeek = converter.FromEntry(entry);
+
+            Assert.IsType<DayOfWeek>(dayOfWeek);
+            Assert.Equal(DayOfWeek.Friday, dayOfWeek);
+        }
+    }
+}
---
+++ MhLabs.AwsDynamoDB/Converters/DynamoEnumConverter.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using System;
+
+namespace MhLabs.AwsDynamoDB.Converters
+{
+    public class DynamoEnumConverter<TEnum> : IPropertyConverter
+    {
+        public object FromEntry(DynamoDBEntry entry)
+        {
+            string valueAsString = entry.AsString();
+            TEnum valueAsEnum = (TEnum)Enum.Parse(typeof(TEnum), valueAsString);
+            return valueAsEnum;
+        }
+
+        public DynamoDBEntry ToEntry(object value)
+        {
+            string valueAsString = value.ToString();
+            DynamoDBEntry entry = new Primitive(valueAsString);
+            return entry;
+        }
+    }
+}
