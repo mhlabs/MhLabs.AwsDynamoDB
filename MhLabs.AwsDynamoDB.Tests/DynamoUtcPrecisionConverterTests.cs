6e4f4c6911526691eb9681a38f3d251129ac01df
Adnan Gohar <adnan.gohar@mathem.se>
2022-06-21 14:54:41 +0200

    fix : added converter for Utc datetime with 3 millisecond precision as dynamodb requires

---
+++ MhLabs.AwsDynamoDB.Tests/DynamoUtcPrecisionConverterTests.cs
@@ -0,0 +1,54 @@
+﻿namespace MhLabs.AwsDynamoDB.Tests
+{
+    using System;
+    using Amazon.DynamoDBv2.DocumentModel;
+    using Converters;
+    using Xunit;
+
+    public class DynamoUtcPrecisionConverterTests
+    {
+        [Fact]
+        public void Should_Create_Null_Date_From_Nullable()
+        {
+            // arrange
+            DateTime? date = null;
+            var converter = new DynamoUtcPrecisionConverter();
+
+            // act
+            var entry = converter.ToEntry(date);
+
+            // assert
+            Assert.IsType<DynamoDBNull>(entry);
+        }
+        
+        [Fact]
+        public void Should_Treat_DateTime_As_Utc()
+        {
+            // arrange
+            var date = new DateTime(2022, 06, 15,07,27,18,419, DateTimeKind.Utc);
+            var converter = new DynamoUtcPrecisionConverter();
+            var expected = "2022-06-15T07:27:18.419Z";
+
+            // act
+            var entry = converter.ToEntry(date);
+
+            // assert
+            Assert.Equal(expected, entry.AsString());
+        }
+        
+        [Fact]
+        public void Should_Create_DateTime_From_Entry()
+        {
+            // arrange
+            DynamoDBEntry entry = new Primitive { Value = "2022-06-15T07:27:18.419Z" };
+            var converter = new DynamoUtcPrecisionConverter();
+            var expected = new DateTime(2022, 06, 15,07,27,18,419, DateTimeKind.Utc);
+
+            // act
+            var date = converter.FromEntry(entry);
+
+            // assert
+            Assert.Equal(expected, date);
+        }
+    }
+}
---
+++ MhLabs.AwsDynamoDB/Converters/DynamoUtcPrecisionConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace MhLabs.AwsDynamoDB.Converters
+{
+    using System;
+    using System.Globalization;
+    using Amazon.DynamoDBv2.DataModel;
+    using Amazon.DynamoDBv2.DocumentModel;
+
+    public class DynamoUtcPrecisionConverter : IPropertyConverter
+    {
+        private const string format = "yyyy-MM-ddThh:mm:ss.fffZ";
+        public DynamoDBEntry ToEntry(object value)
+        {
+            if (value == null)
+                return new DynamoDBNull();
+            return !(value is DateTime dt) ? new DynamoDBNull() :dt.ToString(format);
+        }
+
+        public object FromEntry(DynamoDBEntry entry)
+        {
+            if (!(entry is Primitive primitive))
+            {
+                return null;
+            }
+
+            if (primitive.Type != DynamoDBEntryType.String)
+            {
+                throw new InvalidCastException($"DateTime cannot be converted as its type is {primitive.Type} with a value of {primitive.Value}");
+            }
+
+            return DateTime.ParseExact(primitive.AsString(), format, CultureInfo.InvariantCulture).ToUniversalTime();
+        }
+    }
+}
